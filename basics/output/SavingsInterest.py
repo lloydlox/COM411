4e4430e874acc4ce2b7c19432869c8520175c0db
repl.it user <replituser@example.com>
2020-10-24 01:00:03 +0000

    fix

--- basics/output/SavingsInterest.py
+++ basics/output/SavingsInterest.py
@@ -1,6 +1,6 @@
 TotalSavings = 50
 Print (TotalSavings)
-Interest = 0.5
+Interest = 5
 Print (Interest)
-NewSavings = TotalSavings * Interest
+NewSavings = TotalSavings + Interest
 Print (NewSavings)
