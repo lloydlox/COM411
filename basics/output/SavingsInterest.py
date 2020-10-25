8aca6bf69481499c9df0bcd8195f9a19143000ed
repl.it user <replituser@example.com>
2020-10-25 12:18:51 +0000

    fixed calculation

--- basics/output/SavingsInterest.py
+++ basics/output/SavingsInterest.py
@@ -1,6 +1,6 @@
-TotalSavings = 50
-Print (TotalSavings)
-Interest = 5
-Print (Interest)
-NewSavings = TotalSavings + Interest
-Print (NewSavings)
+TotalSavings = int  (50)
+print (TotalSavings)
+Interest =  (5)
+print (Interest)
+NewSavings = TotalSavings * Interest
+print ('Your NewTotalSavings is',NewSavings)
