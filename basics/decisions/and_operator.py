7c494a59107b1e82dd3768d8b89c4cf4fbb3262e
repl.it user <replituser@example.com>
2020-11-04 23:22:33 +0000

    added the and operator solution

---
+++ basics/decisions/and_operator.py
@@ -0,0 +1,10 @@
+# prompt user for their expriences
+print ("What did I hear?")
+hear = input()
+print ("What did I see?")
+see = input()
+
+if hear == "grr" and see =="two red eyes":
+ print ("There is a scary creature. I should get out her!")
+else:
+  print ("I am little scared but I will continue")
